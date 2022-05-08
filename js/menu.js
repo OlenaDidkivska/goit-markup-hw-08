5ddce7589568abc91e07c4725104e176f4917bea
OlenaDidkivska <elenatyutyuma@gmail.com>
2022-05-08 23:16:59 +0300

    add style tablet

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+
+(() => {
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+
+  menuBtnRef.addEventListener('click', () => {
+    const expanded =
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+    menuBtnRef.classList.toggle('page-header__button--is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+    mobileMenuRef.classList.toggle('page-header__menu--is-open');
+  });
+})();
